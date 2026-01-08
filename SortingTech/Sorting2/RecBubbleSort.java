f65d9ed72d986e2036ef23fc55824e8107e36111
Pranali Habib <pranalihabib@gmail.com>
2026-01-08 15:30:46 +0530

    Rec Bubble and Insertion Sort

--- SortingTech/Sorting2/RecBubbleSort.java
+++ SortingTech/Sorting2/RecBubbleSort.java
@@ -1,5 +1,46 @@
 package SortingTech.Sorting2;
 
 public class RecBubbleSort {
-    
+
+    //brute force
+    // static void bubbleSort(int[] arr, int n){
+    //     if(n==1) return;
+    //     for( int i = 0; i<n-1; i++){
+    //         if(arr[i] > arr[i+1]){
+    //             int temp = arr[i+1];
+    //             arr[i+1] = arr[i];
+    //             arr[i] = temp;
+    //         }
+    //     }
+    //     bubbleSort(arr, n-1);  
+    // }
+
+    //optimal 
+    static void bubbleSort(int[] arr, int n){
+        if(n==1) return;
+        boolean swap = false;
+        for( int i = 0; i<n-1; i++){
+            if(arr[i] > arr[i+1]){
+                int temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+                swap = true;
+            }
+        }
+        if(!swap) return;
+        bubbleSort(arr, n-1);  
+    }
+
+    public static void main(String[] args) {
+        // int[] arr1 = {5,9,1,6,2,8,4,3,7};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        bubbleSort(arr2, arr2.length);
+        for (int i : arr2) {
+            System.out.print(i + " ");
+        }
+    }
 }
+
+// cd "/Users/pranalihabib/Desktop/java "
+// javac SortingTech/Sorting2/RecBubbleSort.java 
+// java SortingTech.Sorting2.RecBubbleSort
--- SortingTech/Sorting2/RecInsertionSort.java
+++ SortingTech/Sorting2/RecInsertionSort.java
@@ -1,5 +1,27 @@
 package SortingTech.Sorting2;
 
 public class RecInsertionSort {
-    
+    static void insertionSort(int arr[], int i, int n){
+        if(n == i ) return;
+        int temp = arr[i];   
+            int j = i-1;    
+            while( j>=0 && arr[j] > temp){
+                arr[j+1] = arr[j];
+                j--;  
+            }
+            arr[j+1] = temp;
+        insertionSort(arr, i+1, n);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {5,9,1,6,2,8,4,3,7};
+        insertionSort(arr1,0, arr1.length);
+        for (int i : arr1) {
+            System.out.print(i + " ");
+        }
+    }
 }
+
+// cd "/Users/pranalihabib/Desktop/java "
+// javac SortingTech/Sorting2/RecInsertionSort.java 
+// java SortingTech.Sorting2.RecInsertionSort
